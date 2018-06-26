2fc7abdc2450234e996f9a3e07a6cfcbc0aed844
Reshuashifa <39693411+Reshuashifa@users.noreply.github.com>
2018-06-26 14:18:40 +0530

    Create pos.java

---
+++ pos.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class pos
+{
+public static vpod main(String args[])
+{
+Scanner sc=nwe Scanner(System.in);
+int a=sc.nextInt();
+if(a<0)
+{
+System.out.println("Negative");
+}
+else
+{
+System.out.println("positive");
+}
+}
+}
